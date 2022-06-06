1aa3af160051d3be5fcc636d42cf2a67180a82b4
黄镇 <huangzhen07@corp.netease.com>
2022-06-06 21:05:28 +0800

    code 303 优化

--- java/mango/listandarray/code303.java
+++ java/mango/listandarray/code303.java
@@ -3,29 +3,26 @@ package mango.listandarray;
 /**
  * 给定一个整数组nums
  * 求出数组从索引 i 到 j (i<=j) 范围内元素的总和，包含i j两点
- *
+ * <p>
  * 前缀和问题，初始化时先计算出前n项和
+ * 注意输入的是数组的下标
  */
 public class code303 {
-    private final int[] rangeSumFromZero;
+    private final int[] preSum;
 
     public code303(int[] nums) {
-        rangeSumFromZero = new int[nums.length];
-        for (int i = 0; i <= nums.length - 1; i++) {
-            int rangeSum = 0;
-            for (int j = 0; j <= i; j++) {
-                rangeSum += nums[j];
-            }
-            rangeSumFromZero[i] = rangeSum;
+        preSum = new int[nums.length + 1];
+        int rangeSum = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            rangeSum += nums[i - 1];
+
+            preSum[i] = rangeSum;
         }
     }
 
     //设计一个sumRange时间复杂度最低的方法
     public int sumRange(int left, int right) {
-        if (left == 0) {
-            return rangeSumFromZero[right];
-        }
-        return rangeSumFromZero[right] - rangeSumFromZero[left - 1];
+        return preSum[right +1] - preSum[left];
     }
 
 }
