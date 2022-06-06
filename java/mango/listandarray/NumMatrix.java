5f98c4984362784288c986c32324dad93985e178
黄镇 <huangzhen07@corp.netease.com>
2022-06-06 21:33:33 +0800

    code 304

---
+++ java/mango/listandarray/NumMatrix.java
@@ -0,0 +1,18 @@
+package mango.listandarray;
+
+/**
+ * 给定一个二维矩阵 matrix，以下类型的多个请求
+ * 1.计算其子矩阵内元素总和，改子矩阵的左上角为(row1, col1)，右下角为(row2, col2)
+ * 实现 NumMatrix 类:
+ * 1.NumMatrix(int[][] matrix) 给定整数矩阵 matrix 进行初始化
+ * 2.int sumRegion(int row1, int col1, int row2, int col2) 返回 左上角 (row1, col1) 、右下角 (row2, col2) 所描述的子矩阵的元素 总和 。
+ */
+public class NumMatrix {
+    public NumMatrix(int[][] matrix) {
+
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return 0;
+    }
+}
