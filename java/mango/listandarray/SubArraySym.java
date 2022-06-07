db8bdcb96d35b6f808a4cc7a8c376e1842fd9bab
黄镇 <huangzhen07@corp.netease.com>
2022-06-07 21:29:35 +0800

    code 560 resolve

---
+++ java/mango/listandarray/SubArraySym.java
@@ -0,0 +1,32 @@
+package mango.listandarray;
+
+/**
+ * code 560
+ * 给一个整数数组 nums 和一个整数 k，统计并返回数组中和为 k 的连续子数组的个数
+ * <p>
+ * nums = [1,1,1], k = 2    result = 2
+ * nums = [1,2,3], k = 2    result = 2
+ * <p>
+ * 思路:先算出来所有的前缀和，然后列举所有的子数组的可能，找出等于k的前缀和的个数
+ */
+public class SubArraySym {
+
+    public int subarraySum(int[] nums, int k) {
+        //记录前缀和
+        int[] preSum = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
+        }
+
+        //遍历前缀和，计算结果
+        int count = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            for (int j = nums.length; j > i; j--) {
+                if (preSum[j] - preSum[i] == k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
