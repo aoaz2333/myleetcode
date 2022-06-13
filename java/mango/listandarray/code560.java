3c7a8ef8e896a9b78cd8d32b981936c3ed25049b
黄镇 <huangzhen07@corp.netease.com>
2022-06-13 21:38:55 +0800

    code 560 solution560

--- java/mango/listandarray/code560.java
+++ java/mango/listandarray/code560.java
@@ -1,5 +1,8 @@
 package mango.listandarray;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * code 560
  * 给一个整数数组 nums 和一个整数 k，统计并返回数组中和为 k 的连续子数组的个数
@@ -34,13 +37,58 @@ public class code560 {
 
     public int subarraySum2(int[] nums, int k) {
         int[] preSum = new int[nums.length + 1];
+        Map<Integer, Integer> preSum2Count = new HashMap<>();
 
         for (int i = 1; i <= nums.length; i++) {
             preSum[i] = preSum[i - 1] + nums[i - 1];
+            int count = preSum2Count.getOrDefault(preSum[i], 0);
+            count++;
+            preSum2Count.put(preSum[i], count);
         }
 
-        HashMap<Integer, Integer>
+        int count = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            int target = preSum[i] + k;
+            count += preSum2Count.getOrDefault(target, 0);
 
-
+            target = preSum[i] - k;
+            count += preSum2Count.getOrDefault(target, 0);
+        }
+        return count;
     }
+
+    /**
+     * 一边遍历，一边在hash表中查询有没有目标结果，简化了内层的时间复杂度
+     */
+    public int subarraySum3(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> preSum2Count = new HashMap<>();
+
+        //初始化0，为了自身之和刚好等于k时，不会被过滤
+        preSum2Count.put(0, 1);
+
+        int count = 0;
+        int sum0Toi = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            sum0Toi += nums[i];
+
+            //注意要先查找有没有合适的结果，再将自身的结果记录到
+            int target = sum0Toi - k;
+            //在前面已有的子数组中
+            count += preSum2Count.getOrDefault(target, 0);
+
+            int preSumCount = preSum2Count.getOrDefault(sum0Toi, 0);
+            preSumCount++;
+            preSum2Count.put(sum0Toi, preSumCount);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        code560 c = new code560();
+        int[] array = {1};
+        int k = 0;
+        System.out.println(c.subarraySum3(array, k));
+    }
+
 }
