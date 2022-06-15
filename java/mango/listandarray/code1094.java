b9033ca4597ca92f9579df23a45f717bc11ec80d
黄镇 <huangzhen07@corp.netease.com>
2022-06-15 20:11:36 +0800

    code 21 solution

---
+++ java/mango/listandarray/code1094.java
@@ -0,0 +1,18 @@
+package mango.listandarray;
+
+/**
+ * 车上最初有 capacity 个空座位，车只能向一个方向行驶（不能掉头或改变方向）
+ *
+ * 给定整数 capacity 和一个数组 trips
+ * trips[i]=[passengerNumber_i, from_i, to_i]表示第 i 次旅行有 passengerNumber_i 名乘客，
+ * 接他们和放他们的位置分别是 from_i 和 to_i，这些位置是从汽车的初始位置向东的公里数
+ * 当且仅当你可以在所有指定的行程中接送所有乘客时，返回 true，否则请返回 false
+ *
+ * 输入：trips = [[2,1,5],[3,3,7]], capacity = 4
+ * 输出：false
+ *
+ * 输入：trips = [[2,1,5],[3,3,7]], capacity = 5
+ * 输出：true
+ */
+public class code1094 {
+}
---
+++ java/mango/listandarray/code21.java
@@ -0,0 +1,80 @@
+package mango.listandarray;
+
+/**
+ * 合并两个升序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+ */
+public class code21 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode newList = null;
+        ListNode nowList = null;
+
+        while (null != list1 && null != list2) {
+            if (list1.val <= list2.val) {
+                if (null == newList) {
+                    newList = list1;
+                    nowList = list1;
+                } else {
+                    nowList.next = list1;
+                    nowList = nowList.next;
+                }
+
+                list1 = list1.next;
+            } else {
+                if (null == newList) {
+                    newList = list2;
+                    nowList = list2;
+                } else {
+                    nowList.next = list2;
+                    nowList = nowList.next;
+                }
+
+                list2 = list2.next;
+            }
+        }
+
+        while (null != list1){
+            if (null == newList) {
+                newList = list1;
+                nowList = list1;
+            } else {
+                nowList.next = list1;
+                nowList = nowList.next;
+            }
+
+            list1 = list1.next;
+        }
+
+        while(null != list2){
+            if (null == newList) {
+                newList = list2;
+                nowList = list2;
+            } else {
+                nowList.next = list2;
+                nowList = nowList.next;
+            }
+
+            list2 = list2.next;
+        }
+
+        return newList;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
