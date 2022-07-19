c943da95575f14663b0cfa7871b5807ac8794dda
mango <2435327811@qq.com>
2022-07-20 01:43:07 +0800

    test commit

--- java/mango/listandarray/code560.java
+++ java/mango/listandarray/code560.java
@@ -1,5 +1,10 @@
 package mango.listandarray;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * code 560
  * 给一个整数数组 nums 和一个整数 k，统计并返回数组中和为 k 的连续子数组的个数
@@ -39,8 +44,24 @@ public class code560 {
             preSum[i] = preSum[i - 1] + nums[i - 1];
         }
 
-//        HashMap<Integer, Integer>
+        Map<Integer, Set<Integer>> expectValue2IndexSet = new HashMap<>();
+        for (int i = 1; i <= nums.length; i++) {
+            int expectValue = preSum[i] - k;
+            if (expectValue >= 0) {
+                expectValue2IndexSet.putIfAbsent(expectValue, new HashSet<>());
+                expectValue2IndexSet.get(expectValue).add(i);
+            }
+        }
 
-        return 0;
+        int count = 0;
+        for (int i = 1; i <= nums.length; i++) {
+            int value = preSum[i];
+            if (expectValue2IndexSet.containsKey(value)) {
+                Set<Integer> indexSet = expectValue2IndexSet.get(value);
+                count += indexSet.contains(value) ? indexSet.size() - 1 : indexSet.size();
+            }
+        }
+
+        return count;
     }
 }
